d15c45bdbcccdbcff940dfe7ca3c3431656d7348
Karthikeyan <karthikeyansreekumar@gmail.com>
2026-05-31 22:28:50 +0530

    feat: add Certifications link to navigation

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -64,6 +64,7 @@ export default function Navigation() {
     { name: 'About', href: '#about' },
     { name: 'Skills', href: '#skills' },
     // { name: 'Projects', href: '#projects' },
+    { name: 'Certifications', href: '#certifications' },
     { name: 'Experience', href: '#experience' },
     { name: 'Contact', href: '#contact' },
   ];
